3e67028036397963923396f2e54b6b0d80571d96
Arun <arunkumar74891@gmail.com>
2025-04-02 21:43:22 +0000

    Changes

--- ch2/ftoc/main.go
+++ ch2/ftoc/main.go
@@ -11,11 +11,11 @@ import "fmt"
 
 func main() {
 	const freezingF, boilingF = 32.0, 212.0
-	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
-	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))   // "212°F = 100°C"
+	fmt.Printf("%g°F = %d°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
+	fmt.Printf("%g°F = %d°C\n", boilingF, fToC(boilingF))   // "212°F = 100°C"
 }
 
-func fToC(f float64) float64 {
+func fToC(f int) int {
 	return (f - 32) * 5 / 9
 }
 
